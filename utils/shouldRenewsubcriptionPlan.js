05b0bf6c795e7dacb9d2588d4a62b280cfe5d004
ibraheem82 <ibraheemomikunle82@gmail.com>
2025-03-16 10:48:41 +0100

    comments was added ...

--- utils/shouldRenewsubcriptionPlan.js
+++ utils/shouldRenewsubcriptionPlan.js
@@ -2,12 +2,9 @@ const shouldRenewSubcriptionPlan = (user) => {
     // *  the current date and time.
 // 2025-03-14T06:28:14.770Z
     const today = new Date();
-
-    // !user?.nextBillingDate - This checks if the user doesn't have a nextBillingDate property or if it's null/undefined. The ?. is the optional chaining operator that prevents errors if user is null/undefined.
     // user?.nextBillingDate <= today - This checks if the user's next billing date is today or has already passed.
+    // if it is less than or equal to the current date (user?.nextBillingDate <= today).
     return !user?.nextBillingDate || user?.nextBillingDate <= today;
 };
 
 module.exports = { shouldRenewSubcriptionPlan };
-const today = new Date();
-console.log(today);
